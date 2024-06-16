9a198c5375b2fa6d0d05f5171091e676615c323f
ivan <vanqel@dtechs.io>
2024-06-17 02:55:40 +0300

    transactional

--- src/main/kotlin/com/api/factory/statistic/service/StatisticService.kt
+++ src/main/kotlin/com/api/factory/statistic/service/StatisticService.kt
@@ -16,9 +16,11 @@ import com.api.factory.statistic.dto.StatsObjectDayMonthYear
 import com.api.factory.statistic.dto.StatsTypeOutput
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate
 
 @Service
+@Transactional
 class StatisticService(
     val usersRepository: IUsersRepository,
     val reportService: IReportService,
